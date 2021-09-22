47d38c1e08067640d3082df218aed7e27dea77c5
Leonardo <leodesouzauy@gmail.com>
2021-09-21 21:04:34 -0300

    Comment build Shell Script.

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 title="# Table of contents\n\n"
 content="$(cat content)\n"
+
+# Readme for GitHub.
 printf "$title$content" >README.md
+
+# Structure summary for GitBook.
 printf "$title* [Table of contents](README.md)\n\n$content" >SUMMARY.md
