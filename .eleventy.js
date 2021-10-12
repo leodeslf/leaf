68c46e7cb255eab5de05f842976a249fbeffe474
Leonardo <leodesouzauy@gmail.com>
2021-10-12 20:54:51 -0300

    Add/Update site files.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.setQuietMode(true);
-  eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.setTemplateFormats("md,njk,css");
+  eleventyConfig.addPlugin(syntaxHighlight);
   return {
     dir: {
       output: 'docs' // Folder for GitHub Pages.
