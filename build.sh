a661d4b715f3503bea39599769234a7ab6494acf
Leonardo <leodesouzauy@gmail.com>
2021-10-31 19:17:27 -0300

    Update build script.

--- build.sh
+++ build.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
-
 content="$(cat content)"
 layout="---\nlayout: default\n---\n"
-toc="# Table of contents\n"
+task="Build Docs & Notes"
+error=0
 
-# GitHub's readme:
-echo -e "$toc\n$content" >README.md
+echo "📦 $task working..."
+# 1) Site's root page
+#   1.1) Use file's names as index's root
+printf -- "$layout## Table of contents\n$(sed 's/.md/\//g' <<<$content)" >index.md &&
+  # 2) Site's content pages
+  #   2.1) Create backup
+  cp -r notes summs backup &&
+  #   2.2) Format files
+  #     2.2.1) Add one level to the heading (keeping code blocks' comments)
+  #     2.2.2) Add one level to the first heading
+  #     2.2.3) Add layout
+  find notes summs -type f -exec sed -i -e 's/## /### /g' -e '1 s/# /## /' -e "1i$layout" {} \; &&
+  #   2.3) Bulid site
+  eleventy &&
+  #   2.4) Remove formatted files
+  rm -r notes summs &&
+  #   2.5) Restore originals
+  mv backup/notes backup/summs ./ &&
+  echo "📦 $task finished." || error=1
 
-# GitBook's structure (aside nav/TOC):
-echo -e "$toc\n* [Table of contents](README.md)\n\n$content" >SUMMARY.md
+if (($error)); then
+  echo "📦 $task failed!" && exit 1
+fi
 
-# 11ty's static index page:
-# Delete empty lines;
-# Add one level to the list;
-# Use 2nd titles as 1st level of the list;
-# Use file name as index's folder.
-printf -- "$layout#$toc$(sed -e '/^$/d' -e 's/* /  * /g' -e 's/## /* /g' -e 's/.md/\//g' <<<$content)" >index.md
-# Create backup.
-cp -r notes summs backup
-# Add one level to the heading (avoiding adding extra hashes to bash comments);
-# Add one level to the first heading;
-# Add basic layout.
-find notes summs -type f -exec sed -i -e 's/## /### /g' -e '1 s/# /## /' -e "1i$layout" {} \;
-# Bulid site. 
-eleventy
-# Remove modified files.
-rm -r notes summs
-# Restore original files from backup.
-mv backup/notes backup/summs ./
-
-echo "Docs & Notes done!"
+# 3) Check and push changes
+while getopts ":g" opt; do
+  case $opt in
+  g | git)
+    {
+      task="Add/Commit/Push changes"
+      echo "🔃 $task working..."
+      #git add . &&
+      #  git commit -m "Add/Commit/Push in build script." &&
+      #  git push &&
+      echo "🔃 $task finished." ||
+        echo "🔃 $task failed!"
+    }
+    ;;
+  esac
+done
