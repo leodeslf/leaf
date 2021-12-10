5c96d5d6806c71ac3bc57e10a7f94b7f86b93982
Leonardo <leodesouzauy@gmail.com>
2021-12-10 13:07:55 -0300

    Improve error handling. Add feedback. Clean code.

--- build.sh
+++ build.sh
@@ -1,47 +1,88 @@
 #!/bin/bash
-content="$(cat content)"
+
+function feedback() {
+  echo -e "🌱 $task: \e[34mStarted...\e[39m"
+}
+
+function success() {
+  echo -e "🌱 $task: \e[92mFinished\e[39m"
+}
+
+function error() {
+  echo -e "🌱 $task: \e[91mFailed!\e[39m" && fail=true
+}
+
 layout="---\nlayout: default\n---\n"
-task="Build"
-error=0
+fail=false
 
-echo "📦 $task started."
-# 1) Site's root page
-#   1.1) Use file's names as index's root
-printf -- "$layout## Table of contents\n$(sed 's/.md/\//g' <<<$content)" >index.md &&
-  # 2) Site's content pages
-  #   2.1) Create backup
-  cp -r notes summs backup &&
-  #   2.2) Format files
-  #     2.2.1) Add one level to the heading (keeping comments in code blocks)
-  #     2.2.2) Add one level to the first heading
-  #     2.2.3) Add layout
-  find notes summs -type f -exec sed -i -e 's/## /### /g' -e '1 s/# /## /' -e "1i$layout" {} \; &&
-  #   2.3) Bulid site
-  eleventy &&
-  #   2.4) Remove formatted files
-  rm -r notes summs index.md &&
-  #   2.5) Restore originals
-  mv backup/notes backup/summs ./ &&
-  echo "📦 $task finished." ||
-  error=1
+task="Building"
+feedback
 
-if (($error)); then
-  echo "📦 $task failed!" && exit 1
-fi
+# 1)
+task="Backup"
+feedback
+cp -r notes summs backup || error
+"$fail" && exit 1
+success
 
-# 3) Check and push changes
+# 2)
+task="Homepage"
+toc="$(cat toc)"
+feedback
+# 2.1) Use file name as index's folder name for each URL
+# 2.2) Add layout
+printf -- "$layout$(sed 's/.md/\//g' <<<$toc)" >index.md || error
+"$fail" && exit 1
+success
+
+# 3)
+task="Content"
+feedback
+# 3.1) Add one level to the heading (keeping comments in code blocks)
+# 3.2) Add one level to the first heading
+# 3.3) Add layout
+find notes summs -type f -exec sed -i -e 's/## /### /g' -e '1 s/# /## /' -e "1i$layout" {} \; || error
+"$fail" && exit 1
+success
+
+# 4)
+task="11ty build"
+feedback
+eleventy || error
+"$fail" && exit 1
+success
+
+# 5)
+task="Remove garbage"
+feedback
+rm -r notes summs index.md || error
+"$fail" && exit 1
+success
+
+# 6)
+task="Restore originals"
+feedback
+mv backup/notes backup/summs ./ || error
+"$fail" && exit 1
+success
+
 while getopts ":g" opt; do
   case $opt in
   g | git)
     {
-      task="Push changes"
-      echo "🔃 $task started."
       git add docs
-      git commit -m "Update build folder from build script."
-      git push &&
-        echo "🔃 $task finished." ||
-        echo "🔃 $task failed!"
+      git commit -m "Update & Commit from build script."
+
+      # 7)
+      task="Push changes"
+      feedback
+      git push || error
+      "$fail" && exit 1
+      success
     }
     ;;
   esac
 done
+
+task="Building"
+success
